f7f9e2735359dcc7d94e98f2f42d095e2480427d
azharprabudi <azharprabui@gmail.com>
2019-12-29 12:43:00 +0700

    test auto deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import "../styles/pages/homepage.styl"
 const IndexPage = () => (
   <div className="container is-fluid">
     <div className="columns">
-      <div className="column is-full-mobile is-half-desktop">Hello</div>
-      <div className="column is-full-mobile is-half-desktop">Hello</div>
+      <div className="column is-full-mobile is-half-desktop">Hello World</div>
+      <div className="column is-full-mobile is-half-desktop">Hello World</div>
     </div>
   </div>
 )
